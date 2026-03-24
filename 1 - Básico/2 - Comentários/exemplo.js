7d0a5cf01d9d3928c80bb46daae48cad7b69f51d
Bruna A. <130995251+moreandmorebr394@users.noreply.github.com>
2026-03-24 17:48:21 -0300

    Create exemplo.js

---
+++ 1 - Básico/2 - Comentários/exemplo.js
@@ -0,0 +1,3 @@
+// É assim que é feito comentários em Javascript.
+// Importante para realizar documentação e o que está acontecendo no código, tanto para o criador, pois os códigos acabam ficando longos
+// tanto para quem pega o código pela metade.
